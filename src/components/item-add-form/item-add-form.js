10172637845966a18551b93bcd24bd392a62d7c0
Vladislav <k0tjaka@yandex.ru>
2020-03-01 20:45:52 +0300

    Fix form layout

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -20,22 +20,24 @@ export default class ItemAddForm extends Component {
         this.props.onItemAdded(this.state.label);
         this.setState({
             label: ''
-        });  
+        });
     };
 
     render() {
         return (
-            <form className="item-add-form d-flex"
-                  onSubmit={this.onSubmit}>
+            <form className="item-add-form input-group"
+                onSubmit={this.onSubmit}>
 
                 <input type="text"
-                       className="form-control"
-                       onChange={this.onLabelChange}
-                       placeholder="What needs to be done" 
-                       value={this.state.label} />
-                <button className="btn btn-outline-secondary">
-                Add item
-                </button>
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                    value={this.state.label} />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary">
+                        Add item
+                    </button>
+                </div>
             </form>
         )
     }
